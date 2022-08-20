b0c08df88b9b65f558a6880b51e9e5347915184f
Denver0619 <janjan12345678910111213141516@gmail.com>
2022-08-20 23:54:09 +0800

    new layout and color scheme (still clunky ui)

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var menuBtn = document.querySelector('#menu');
 var navBar = document.querySelector('nav');
+var navList = document.querySelector('nav ul');
+var navCount = navList.childElementCount;
+console.log(navCount);
 let isOpen = false;
 
+// menu animation
 menuBtn.addEventListener('click', () => {
-    if(!isOpen) {
+    if (!isOpen) {
         menuBtn.classList.add('open');
         navBar.classList.add('active');
         isOpen = true;
